1c0f51499aba55e12c7c736be4af903fdec7c701
Nelson Klutse <1698197+azumah@users.noreply.github.com>
2021-07-12 10:04:56 +0000

    Initial Commit

---
+++ SwUIDemo/SwUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUIDemoApp.swift
+//  SwUIDemo
+//
+//  Created by Azu on 12/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
